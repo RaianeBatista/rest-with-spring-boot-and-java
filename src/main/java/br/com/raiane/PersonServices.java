43c927f5bb6d2c8f86d1b2cf609470e1164c8d87
RaianeBatista <202311169804@alunos.estacio.br>
2026-06-13 23:57:15 -0300

    feat: add Person model, PersonController, and PersonServices for managing person data

---
+++ src/main/java/br/com/raiane/PersonServices.java
@@ -0,0 +1,55 @@
+package br.com.raiane;
+
+import br.com.raiane.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+
+    public List<Person> findAll() {
+        logger.info("Finding all person!");
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
+    public Person findById(String id) {
+        logger.info("Finding onde Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Raiane");
+        person.setLastName("Batista");
+        person.setAddress("Itaituba - Pará - Brasil");
+        person.setGender("Female");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some Address in Brasil");
+        person.setGender("Female");
+        return person;
+    }
+}
--- src/main/java/br/com/raiane/model/Greeting.java
+++
@@ -1,5 +0,0 @@
-package br.com.raiane.model;
-
-public record Greeting(long id, String content) {
-
-}
